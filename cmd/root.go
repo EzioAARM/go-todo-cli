8e82f5caac56a0eee804f77df328d2612f55039e
Axel Rodriguez <alejandrom9712@gmail.com>
2025-12-05 14:09:45 -0600

    Upload project

--- cmd/root.go
+++ cmd/root.go
@@ -6,36 +6,29 @@ package cmd
 import (
 	"os"
 
+	"github.com/ezioaarm/go-todo-cli/cmd/add"
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
+// RootCmd represents the base command when called without any subcommands
+var RootCmd = &cobra.Command{
 	Use:   "todo",
-	Short: "Application to manage todo lists",
-	Long:  `Application to create, update, delete todo lists and tasks.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "CLI to manage tasks in real time",
+	Long: `Modern CLI to manage tasks in real time.
+	
+This CLI has support for multiple database engines (PostgreSQL, CockroachDB, Neo4j).`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	err := rootCmd.Execute()
+	err := RootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
 	}
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.go-todo-cli.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	RootCmd.AddCommand(add.AddCmd)
+	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
